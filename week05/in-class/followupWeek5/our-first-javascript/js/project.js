5bcc32db2212d7c7824b5f988fb1e571a427303f
iuloshi <iuloshi@gmail.com>
2014-01-21 20:58:25 -0500

    portfolio wireframes and followup files for week 5 in-class exercises

---
+++ week05/in-class/followupWeek5/our-first-javascript/js/project.js
@@ -0,0 +1,65 @@
+// alert("12");
+
+// alert(1+2);
+
+// alert(32-6);
+
+// alert(12*2);
+
+// alert(24/2);
+
+
+//Alerts
+// alert(5*23);
+// alert(4-2);
+// alert(42/6);
+// alert(7+5);
+// alert("Joe Bliss");
+// alert("The end is near!");
+
+// var addNum; // Declaration
+// addNum = 2+3; //Assignment
+// alert(addNum);
+
+// var subtractNum;
+// subtractNum = addNum-1;
+// alert(subtractNum);
+
+// var bigNum = 42/6+24-1277/104-4; //Initialization
+// alert(bigNum);
+
+// var reassignMe = "Joe";
+// reassignMe = "Eddie";
+// reassignMe = "Constantin";
+// reassignMe = "Dario";
+
+// alert(reassignMe);
+
+/***** EXAMPLE OF REASSIGNMENT *****/
+
+var reassignMe = "Joe";
+
+var addNum; // Declaration
+addNum = 2+3; //Assignment
+alert(reassignMe+"'s favorite number is "+addNum);
+
+reassignMe = "Eddie";
+
+var subtractNum;
+subtractNum = addNum-1;
+alert(reassignMe+"'s favorite number is "+subtractNum);
+
+reassignMe = "Constantin";
+
+var bigNum = 42/6+24-1277/104-4; //Initialization
+alert(reassignMe+"'s favorite number is "+bigNum);
+
+reassignMe = "Dario";
+
+alert(reassignMe+"'s favorite number is ... a mystery!");
+
+
+
+
+
+
